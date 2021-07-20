e88b221cf980477b2f60044343ce491d1397c030
Richard Clark <clarkrd@icloud.com>
2021-07-20 16:30:28 +0100

    Initial Commit

---
+++ QcardsV2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QcardsV2
+//
+//  Created by Richard Clark on 20/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QcardsV2/QcardsV2App.swift
@@ -0,0 +1,17 @@
+//
+//  QcardsV2App.swift
+//  QcardsV2
+//
+//  Created by Richard Clark on 20/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct QcardsV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
