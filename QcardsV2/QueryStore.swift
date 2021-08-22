6dd94bcf88157b47523b4829980ef9fb11658680
Richard Clark <clarkrd@icloud.com>
2021-08-22 16:07:32 +0100

    QueryStore added, builds and runs fine

---
+++ QcardsV2/QueryStore.swift
@@ -0,0 +1,75 @@
+//
+//  QueryStore.swift
+//  QcardsV2
+//
+//  Created by Richard Clark on 22/08/2021.
+//
+
+import Foundation
+import CoreData
+import SwiftUI
+
+///folowing is added from Donny Waals "fetching objects from core data in swiftUI."
+//This is a copy of TopicStore pretty much...
+
+class QueryStore: NSObject, ObservableObject {
+    @Published var queries: [Query] = []
+    private let queriesController: NSFetchedResultsController<Query>
+    
+    //need to add an optional topic, then pass that in to use as a predicate in the fetch request...
+    init( managedObjectContext: NSManagedObjectContext) {
+        queriesController = NSFetchedResultsController(fetchRequest: Query.fetchRequest(),
+                                                      managedObjectContext: managedObjectContext,
+                                                      sectionNameKeyPath: nil,
+                                                      cacheName: nil
+        )
+
+       var fetchRequest: NSFetchRequest<Query> {
+            let request: NSFetchRequest<Query> = Query.fetchRequest()
+            request.sortDescriptors = [NSSortDescriptor(keyPath: \Query.queryQuestion, ascending: false)]
+            return request
+        }
+        
+        
+        
+        
+        
+        
+        //PLAN1
+        //create a new queryController here which uses a local fetch request
+        //create the fetch request using a predicate topic, which is passed in, sort descriptor not needed
+        //run the actual perform fetch and assign results to the published var queries
+        super.init()
+        queriesController.delegate = self
+
+        do {
+            try queriesController.performFetch()
+            print("TopicStore ran topicsController.performFetch()")
+            queries = queriesController.fetchedObjects ?? []
+        }   catch {
+            print("failed to fetch")
+        }
+        
+    }
+    
+    
+    
+}
+
+//not sure if either or both of these are needed
+extension QueryStore: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        guard let changedQueries = controller.fetchedObjects as? [Query]
+        else { return }
+        print("TopicStore ran controllerDidChangeContent")
+        queries = changedQueries
+    }
+}
+extension QueryStore {
+    func controllerDidChangeObject(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+        guard let changedQueries = controller.fetchedObjects as? [Query]
+        else { return }
+        print("TopicStore ran controllerDidChangeObject")
+        queries = changedQueries
+    }
+}
